c53edbb74477a72d21b76997998bbe343f7d7629
miguel lopez <mlopez383@gmail.com>
2012-12-01 20:15:36 -0400

    Revert "Revert "modulos inicial para planear un viaje seguro""
    This reverts commit 220d75b0f23d2259e7d95f4877d7fcf2a9fc33db.

---
+++ sites/all/modules/touristic_resources/touristic_resource.tpl.php
@@ -0,0 +1,12 @@
+<div align="justify">
+<?php print $rt_brief_description ?>
+</div>
+<br />
+
+<div align="justify">
+<?php print $rt_details ?>
+</div>
+
+<div>
+<b><?php print $rt_name; ?></b>
+</div>
