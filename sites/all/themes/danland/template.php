220d75b0f23d2259e7d95f4877d7fcf2a9fc33db
miguel lopez <mlopez383@gmail.com>
2012-12-01 20:12:45 -0400

    Revert "modulos inicial para planear un viaje seguro"
    This reverts commit dd982172537390d56586d65c737513089dd43e96.

--- sites/all/themes/danland/template.php
+++ sites/all/themes/danland/template.php
@@ -14,22 +14,8 @@ function danland_page_class($sidebar_first, $sidebar_second) {
 }
 
 function danland_preprocess_html(&$vars) {
-  	// Add conditional CSS for IE6.
-	drupal_add_css(path_to_theme() . '/style.ie6.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'IE 6', '!IE' => FALSE), 'preprocess' => FALSE));
-	
-	//js for plan_trip
-	if (strpos($_SERVER['REQUEST_URI'],'/planning/')) {
-		drupal_add_css('sites/all/modules/plan_trip/plan_trip.css');
-		drupal_add_js('https://maps.google.com/maps/api/js?sensor=false');
-		drupal_add_js('sites/all/modules/plan_trip/plan_trip_googlemaps.js');
-		drupal_add_js('sites/all/modules/plan_trip/infobox.js');
-		
-		drupal_add_js('misc/ui/jquery.ui.core.min.js');
-		drupal_add_js('misc/ui/jquery.ui.widget.min.js');
-		drupal_add_js('misc/ui/jquery.ui.mouse.min.js');
-		drupal_add_js('misc/ui/jquery.ui.draggable.min.js');
-		drupal_add_js('misc/ui/jquery.ui.droppable.min.js');
-	}
+  // Add conditional CSS for IE6.
+drupal_add_css(path_to_theme() . '/style.ie6.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'IE 6', '!IE' => FALSE), 'preprocess' => FALSE));
 }
 
 function danland_preprocess_maintenance_page(&$variables) {
